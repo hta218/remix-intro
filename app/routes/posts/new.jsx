813149f5bdf963f9e4b2a2a5917854c3656e8645
Leo Huynh <huynhtuananh21895@gmail.com>
2021-12-19 15:35:05 +0700

    Add simple action

--- app/routes/posts/new.jsx
+++ app/routes/posts/new.jsx
@@ -1,10 +1,40 @@
+import { Link, redirect } from "remix"
+
+export const action = async ({ request }) => {
+  const form = await request.formData()
+  const title = form.get('title')
+  const body = form.get('body')
+  console.log("=========> - title", title)
+  console.log("=========> - body", body)
+
+  return redirect('/posts')
+}
+
 function NewPost() {
   return (
-    <div>
+    <>
       <div className="page-header">
         <h1>New post</h1>
+        <Link to="/posts" className="btn btn-reverse">
+          Back
+        </Link>
       </div>
-    </div>
+      <div className="page-content">
+        <form method="POST">
+          <div className="form-control">
+            <label htmlFor="title">Title</label>
+            <input type="text" name="title" id="title" />
+          </div>
+          <div className="form-control">
+            <label htmlFor="body">Post body</label>
+            <textarea name="body" id="body" cols="30" rows="10"></textarea>
+          </div>
+          <button type="submit" className="btn btn-block">
+            Add post
+          </button>
+        </form>
+      </div>
+    </>
   )
 }
 
